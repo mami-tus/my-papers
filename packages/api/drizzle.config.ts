5eda67f40e1830a6af4351d830c04f76bc7ba920
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-05-04 19:54:48 +0900

    apiディレクトリでnpx drizzle-kit generate実行

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/schema.ts',
-  out: './migrations',
+  schema: './src/db/schema.ts',
+  out: './drizzle',
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
