736dddf8cdd87fa11a6a3c988f75767f1918ac00
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-05-05 11:44:53 +0900

    新しい論文テーブルを作成し、ユーザーテーブルの作成日時を追加しました。また、マイグレーションジャーナルとスナップショットを更新しました。#5

--- packages/api/migrations/0000_dashing_sinister_six.sql
+++ packages/api/migrations/0000_dashing_sinister_six.sql
@@ -7,8 +7,14 @@ CREATE TABLE `papers` (
 	`year` integer NOT NULL,
 	`authors` blob,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `unique_user_field_doi` ON `papers` (`user_id`,`field`,`doi`);--> statement-breakpoint
-ALTER TABLE `users` ADD `created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL;
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL
+);
--- packages/api/src/db/schema.ts
+++ packages/api/src/db/schema.ts
@@ -7,13 +7,21 @@ import {
   unique,
 } from 'drizzle-orm/sqlite-core';
 
+const timestamps = {
+  createdAt: text()
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text('updated_at')
+    .notNull()
+    .default(sql`(current_timestamp)`)
+    .$onUpdateFn(() => sql`(current_timestamp)`),
+};
+
 export const users = sqliteTable('users', {
   // システムを拡張したりする段階で 、 必要に応じてULIDへの変更を検討する
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
-  createdAt: text()
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
+  ...timestamps,
 });
 
 export const papers = sqliteTable(
@@ -28,9 +36,7 @@ export const papers = sqliteTable(
     title: text().notNull(),
     year: integer().notNull(),
     authors: blob({ mode: 'json' }).$type<string[]>(),
-    createdAt: text()
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`),
+    ...timestamps,
   },
   (table) => [
     unique('unique_user_field_doi').on(table.userId, table.field, table.doi),
