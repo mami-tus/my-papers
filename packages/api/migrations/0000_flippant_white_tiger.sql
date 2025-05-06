911ac690d4f89f102299576b2d96ae1b3c2b115e
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-05-06 14:01:45 +0900

    マイグレーション実行

---
+++ packages/api/migrations/0000_flippant_white_tiger.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `fields` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`name` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `fields_userId_name_unique` ON `fields` (`user_id`,`name`);--> statement-breakpoint
+CREATE TABLE `papers` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`field_id` integer NOT NULL,
+	`doi` text NOT NULL,
+	`title` text NOT NULL,
+	`year` integer,
+	`month` integer,
+	`day` integer,
+	`authors` blob,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`field_id`) REFERENCES `fields`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `papers_userId_fieldId_doi_unique` ON `papers` (`user_id`,`field_id`,`doi`);--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT (current_timestamp) NOT NULL
+);
